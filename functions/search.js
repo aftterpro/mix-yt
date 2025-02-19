730fc86156fee5a6920056356bb8c389f41f325a
aftterpro <edwin.chiquito@epn.edu.ec>
2025-02-18 22:11:57 -0500

    Update search.js

--- functions/search.js
+++ functions/search.js
@@ -5,8 +5,11 @@ exports.handler = async (event) => {
     const query = event.queryStringParameters.q;
 
     try {
-        const response = await fetch(`https://piped.nosebs.ru/results?search_query=${encodeURIComponent(query)}`);
-
+    const response = await fetch(`https://piped.nosebs.ru/results?search_query=${encodeURIComponent(query)}`, {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    });
         if (!response.ok) {
             const errorText = await response.text();
             console.error(`HTTP error! status: ${response.status} - ${errorText}`);
