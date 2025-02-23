6a071375c5944f2b6425f4e7bbd71edb998d6f94
aftterpro <edwin.chiquito@epn.edu.ec>
2025-02-23 15:57:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -651,7 +651,6 @@ function playFirstVideo() {
 // Variables para el estado del reproductor
 let contenedorPlayer = document.getElementById("contenedor-player");
 let botonExpandir = document.getElementById("botonExpandir");
- let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
  let videoContainer = document.querySelector(".video-container");
 
