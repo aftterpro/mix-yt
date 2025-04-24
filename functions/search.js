f896060425b376699da2d6e5c08001791f6a3705
aftterpro <edwin.chiquito@epn.edu.ec>
2025-04-24 12:07:44 -0500

    Update search.js

--- functions/search.js
+++ functions/search.js
@@ -1,6 +1,5 @@
 //Instancias api
 const pipedInstances = [
-  "https://pipedapi.orangenet.cc",
   "https://api.piped.private.coffee",
     "https://pipedapi.reallyaweso.me",
     "https://pipedapi.ducks.party"
