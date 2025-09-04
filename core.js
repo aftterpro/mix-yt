4729b2ff8f160c0205d503819085ab4ab42d987f
aftterpro <edwin.chiquito@epn.edu.ec>
2025-09-03 19:19:34 -0500

    Update core.js

--- core.js
+++ core.js
@@ -945,7 +945,7 @@ class UnifiedCore {
 
     async getPlaylistInfo(playlistId) {
         const pipedInstances = [
-            "https://piped.private.coffee"
+            "https://api.piped.private.coffee"
         ];
         
         const instanceUrl = pipedInstances[Math.floor(Math.random() * pipedInstances.length)];
