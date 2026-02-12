51e7279d6a692841683c901415e67535fc1d4aa5
aftterpro <edwin.chiquito@epn.edu.ec>
2026-02-12 08:10:40 -0500

    Update youtube-client.js

--- youtube-client.js
+++ youtube-client.js
@@ -1,6 +1,6 @@
 class YouTubeSimplifiedClient {
     constructor() {
-       this.baseUrl = "http://150.230.81.137:3000/search";
+       this.baseUrl = "https://94cc-150-230-81-137.ngrok-free.app/search";
         this.maxRetries = 3;
         this.retryDelay = 1000;
         
@@ -111,7 +111,7 @@ async cargarSegmentos(videoId) {
         }
 
         const userId = 'gaDZcHFATqVfqCtNlv3xGMP6bkrNnKkEHyUd';
-        const apiUrl = `http://150.230.81.137:3000/sponsorblock?videoId=${videoId}`;
+        const apiUrl = `https://94cc-150-230-81-137.ngrok-free.app/sponsorblock?videoId=${videoId}`;
     try {
         // ✅ PROMESA CON TIMEOUT AUMENTADO
         const fetchWithTimeout = new Promise((resolve, reject) => {
