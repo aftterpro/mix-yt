6e9b2c5761fc2e6f9fea80292d7080fa06ca7642
aftterpro <edwin.chiquito@epn.edu.ec>
2026-02-11 21:36:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Módulo: Configuración y Variables Globales
 const CONFIG = {
     origin: window.location.origin, 
-    apiBase: "/search"
+    apiBase: "http://150.230.81.137:3000/search"
 };
 const CROSSFADE_DURATION = 15; // Duración del crossfade en segundos
 let player1, player2;
