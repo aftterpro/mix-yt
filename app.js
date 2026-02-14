aa6313348922dfa6a3fc791a169bb1e4e089de8b
aftterpro <edwin.chiquito@epn.edu.ec>
2026-02-13 20:28:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Módulo: Configuración y Variables Globales
 const CONFIG = {
     origin: window.location.origin, 
-    apiBase: "https://94cc-150-230-81-137.ngrok-free.app/search"
+    apiBase: "https://sphenographic-johnie-supersensually.ngrok-free.dev/search"
 };
 const CROSSFADE_DURATION = 15; // Duración del crossfade en segundos
 let player1, player2;
