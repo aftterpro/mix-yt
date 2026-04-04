8637466f3075513115efcd4c09abc53d301bb0ad
EDWIN JAIRO CHIQUITO UNTUNA <edwin.chiquito@epn.edu.ec>
2026-04-04 12:32:54 -0500

    Update lyrics fetch URL to new IP address

--- public/app.js
+++ public/app.js
@@ -1552,7 +1552,7 @@ class LyricsManager {
         const ctrl = new AbortController();
         setTimeout(() => ctrl.abort(), 7000);
 
-        const res = await fetch(`http://150.230.81.137:5000/get-lyrics?id=${videoId}`, { signal: ctrl.signal });
+        const res = await fetch(`http://20.63.18.153:5000/get-lyrics?id=${videoId}`, { signal: ctrl.signal });
         const data = await res.json();
         if (data.status !== 'success' || !data.data) throw new Error('Oracle: no encontrado');
         return { syncedLyrics: data.data, plainLyrics: data.data.replace(/\[.*?\]/g, ''), provider: 'YT-Subtitles' };
